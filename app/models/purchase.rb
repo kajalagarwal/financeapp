aff81ca54a6d2668e4dc6e607f162ac63c70db4d
kajalagarwal <kajalagarwal.7@gmail.com>
2015-07-19 20:39:25 +0000

    Update Doc

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,7 +3,7 @@ class Purchase < ActiveRecord::Base
   require 'csv'
     validates(:ItemPrice,:PurchaseQuantity,:ItemDescription,:PurchaserName,:MerchantAddress,:MerchantName, presence: true)
     def self.import(file)
-      CSV.foreach(file.path, headers: true) do |row|
+      CSV.foreach(file.path,headers: true) do |row|
         Purchase.create! row.to_hash
       end
     end
