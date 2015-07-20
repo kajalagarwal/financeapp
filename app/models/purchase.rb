421c46fd1943b168075f6cbfea2cc1a974b9e546
kajalagarwal <kajalagarwal.7@gmail.com>
2015-07-20 06:27:50 +0000

    commit the changes for header files

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,8 +4,9 @@ class Purchase < ActiveRecord::Base
     validates(:ItemPrice,:PurchaseQuantity,:ItemDescription,:PurchaserName,:MerchantAddress,:MerchantName, presence: true)
      
      def self.import(file)
-      CSV.foreach(file.path,headers: true) do |row|
-        Purchase.create! row.to_hash
+      CSV.foreach(file.path,headers: false) do |row|
+        row=row.to_a
+        Purchase.create!(ItemPrice: row[3],PurchaseQuantity: row[2],ItemDescription: row[1],PurchaserName: row[0],MerchantAddress: row[4],MerchantName: row[5])
       end
      end
     
