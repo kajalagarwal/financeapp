a1c5e244669fcc10de6587014110d96fe05e5fc3
kajalagarwal <kajalagarwal.7@gmail.com>
2015-07-27 03:38:51 +0000

    Adding a record from the page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,6 +6,7 @@ class PurchasesController < ApplicationController
     @purchases1=Purchase.all
     @user=User.all
     @current_user||=User.find_by(id: session[:user_id])
+    @purch=Purchase.new
   end
 
   def import
@@ -16,6 +17,19 @@ class PurchasesController < ApplicationController
        redirect_to purchases_path, notice: "Invalid CSV file format. Please check the file for header match."
     end
   end
+   def new
+    @purch=Purchase.new
+   end
+   def create
+    @purch = Purchase.create!(purch_params)
+   end
+
+  private
+
+    def purch_params
+      params.require(:purchase).permit(:PurchaserName, :ItemDescription, :ItemPrice,
+                                   :MerchantAddress,:MerchantName,:PurchaseQuantity)
+    end
 
 end
 
